7cbcef595e1594a35d0a2cf990e1cf9019cff71e
Khapov Roman <Khapov.Roman@gmail.com>
2021-11-24 23:58:28 +0300

    Модиф. функции renderTask и чекбокс

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 
 //inputTodo = document.querySelector(".new-todo");
 const task = { id: "1", text: "выучить html", completed: true };
+const tasksList = [
+    { id: "1", text: "выучить html", completed: true },
+    { id: "2", text: "выучить css", completed: true },
+    { id: "3", text: "выучить js", completed: false },
+    { id: "4", text: "выучить фреймворк", completed: false },
+    { id: "5", text: "написать несколько учебных проектов", completed: false },
+    { id: "6", text: "пройти собеседование", completed: false },
+    { id: "7", text: "получить работу", completed: false }
+    ];
+
 const todoList = document.querySelector(".todo-list");
-/*
-function renderTask(newObject) {
-    createListItem(newObject);
-    todoList.append(li);
-    li.append(div);
-    div.append(input);
-    div.append(label);
-    div.append(button);
+
+function renderTasks(newObject) {
+    for(let i = 0; i < newObject.length; i++) {
+        createListItem(newObject[i]);
+    }
+    
+    
  
-}*/
+}
 
 function createListItem(obj) {
     let li = document.createElement('li');
@@ -24,6 +33,7 @@ function createListItem(obj) {
     let input = document.createElement('input');
     input.className = "toggle";
     input.type = "checkbox";
+    input.checked = obj.completed;
     div.append(input);
     let label = document.createElement('label');
     label.textContent = obj.text;
@@ -32,4 +42,4 @@ function createListItem(obj) {
     button.className = "destroy";
     div.append(button);
 }
-createListItem(task);
+renderTasks(tasksList);
