3539417a18dc8ae0408ed5da0bcdd075affe6881
Khapov Roman <Khapov.Roman@gmail.com>
2021-11-25 23:45:17 +0300

    добавление задачи из окна

--- index.js
+++ index.js
@@ -2,24 +2,40 @@
 //inputTodo = document.querySelector(".new-todo");
 const task = { id: "1", text: "выучить html", completed: true };
 const tasksList = [
-    { id: "1", text: "выучить html", completed: true },
+/*     { id: "1", text: "выучить html", completed: true },
     { id: "2", text: "выучить css", completed: true },
     { id: "3", text: "выучить js", completed: false },
     { id: "4", text: "выучить фреймворк", completed: false },
     { id: "5", text: "написать несколько учебных проектов", completed: false },
     { id: "6", text: "пройти собеседование", completed: false },
-    { id: "7", text: "получить работу", completed: false }
+    { id: "7", text: "получить работу", completed: false } */
     ];
 
 const todoList = document.querySelector(".todo-list");
+const newTodo = document.querySelector(".new-todo")
 
-function renderTasks(newObject) {
-    for(let i = 0; i < newObject.length; i++) {
-        createListItem(newObject[i]);
-    }
-    
-    
- 
+function renderTasks(arr) {
+    /* for(let i = 0; i < arr.length; i++) {
+        createListItem(arr[i]); // рисует весь массив сразу, и потом дублирует 
+    } */                        //при добавлении новой задачи
+    let idObject = arr.length - 1;
+    createListItem(arr[idObject]); //рисует один таск
+}
+
+function getId() {
+    let idValue = tasksList.length + 1;
+    return idValue;
+}
+
+function createNewTask() {
+    if (newTodo.value !== "") {
+        let obj = {
+        id : getId(), text : newTodo.value, completed : false
+        };
+    tasksList.push(obj);
+    renderTasks(tasksList);
+    newTodo.value = "";
+    };
 }
 
 function createListItem(obj) {
@@ -41,5 +57,4 @@ function createListItem(obj) {
     let button = document.createElement('button');
     button.className = "destroy";
     div.append(button);
-}
-renderTasks(tasksList);
+};
